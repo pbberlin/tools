cc3e61d4dfb5313e72e72a1ea05fe6e7dda42d9c
Peter Buchmann <peter.buchmann@web.de>
2015-10-08 20:59:48 +0200

    client side browser based oauth2 signin 0.7

--- oauthpb/user.go
+++ oauthpb/user.go
@@ -2,15 +2,24 @@ package oauthpb
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/pbberlin/tools/net/http/loghttp"
+	"github.com/pbberlin/tools/stringspb"
 
 	"appengine"
 	"appengine/user"
 )
 
 func login(w http.ResponseWriter, r *http.Request) {
+
 	c := appengine.NewContext(r)
 	u := user.Current(c)
+	// u, _ := user.CurrentOAuth(c, scope)
+
 	if u == nil {
 		url, err := user.LoginURL(c, r.URL.String())
 		if err != nil {
@@ -20,8 +29,16 @@ func login(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Location", url)
 		w.WriteHeader(http.StatusFound)
 		return
+	} else {
+		// this gets never executed on dev server
+		fmt.Fprintf(w, "Hello, %v, %v, %v, %v!<br>\n", u, u.ID, u.Email, u.FederatedIdentity)
+		url2, err := user.LogoutURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		fmt.Fprintf(w, "<a href='%v'>logout</a>", url2)
 	}
-	fmt.Fprintf(w, "Hello, %v, %v, %v, %v!", u, u.ID, u.Email, u.FederatedIdentity)
+
 }
 
 func Auth(r *http.Request) (bool, *user.User, string) {
@@ -29,7 +46,16 @@ func Auth(r *http.Request) (bool, *user.User, string) {
 	msg := ""
 	c := appengine.NewContext(r)
 
-	u := user.Current(c)
+	u1 := user.Current(c)
+	u2, err := user.CurrentOAuth(c, "")
+	if err != nil {
+		log.Printf("oauth user err %v", err)
+	}
+	u := u1
+	if u == nil {
+		u = u2
+	}
+
 	if appengine.IsDevAppServer() {
 		return true, u, "Logon always shines on DEV system."
 	}
@@ -43,15 +69,95 @@ func Auth(r *http.Request) (bool, *user.User, string) {
 		msg += "google oauth required"
 		return false, nil, msg
 	}
-	if u.ID != "108853175242330402880" && u.ID != "S-1-5-21-2175189548-897864986-1736798499-1000" {
-		msg += "you need to be me; not " + u.ID
-		return false, u, msg
-	}
+
+	log.Printf("is admin: %v", u.Admin)
+
+	// if u.ID != "108853175242330402880" && u.ID != "S-1-5-21-2175189548-897864986-1736798499-1000" {
+	// 	msg += "you need to be me; not " + u.ID
+	// 	return false, u, msg
+	// }
 
 	return true, u, msg
 
 }
 
+func myLookupKey(k interface{}) (interface{}, error) {
+	return k, nil
+}
+
+func TokenSignin(w http.ResponseWriter, r *http.Request) {
+
+	lg, _ := loghttp.BuffLoggerUniversal(w, r)
+
+	// w.Header().Set("Access-Control-Allow-Origin", "http://localhost:1313")
+
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8087")
+
+	w.Header().Del("Access-Control-Allow-Origin")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// err := r.ParseMultipartForm(1024 * 1024 * 2)
+	err := r.ParseForm()
+	lg(err)
+
+	myToken := r.Form.Get("idtoken")
+	tokSize := fmt.Sprintf("Len of Tok was %v. \n", len(myToken))
+
+	// we can also verify the token from here:
+	// https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=XYZ123
+
+	fc1 := func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+
+		log.Printf("algo header is %v\n", token.Header["alg"])
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return myLookupKey(token.Header["kid"])
+	}
+
+	token, err := jwt.Parse(myToken, fc1)
+
+	if err != nil && strings.Contains(err.Error(), jwt.ErrInvalidKey.Error()) {
+		w.Write([]byte("The submitted RSA Key was somehow unparseable. We still accept the token.\n"))
+		/*
+			https://developers.google.com/identity/sign-in/web/backend-auth
+		*/
+		err = nil
+		token.Valid = true
+	}
+
+	if err != nil {
+		w.Write([]byte(err.Error() + ".\n"))
+	}
+
+	if err == nil && token.Valid {
+
+		tk := ""
+		tk += fmt.Sprintf("     Algor:     %v\n", token.Method)
+		tk += fmt.Sprintf("     Header:    %v\n", token.Header)
+		for k, v := range token.Claims {
+			tk += fmt.Sprintf("\t  %-8v %v\n", k, v)
+		}
+		lg(tk)
+
+		w.Write([]byte("tokensignin; valid.   \n"))
+		w.Write([]byte(tokSize))
+		sb := "header-sub-not-present"
+		if _, ok := token.Claims["sub"]; ok {
+			sb = token.Claims["sub"].(string)
+		}
+		w.Write([]byte("ID is " + sb + "\n"))
+
+	} else {
+		w.Write([]byte("tokensignin; INVALID. \n"))
+		w.Write([]byte(tokSize))
+		w.Write([]byte(stringspb.ToLen(myToken, 30)))
+	}
+
+}
+
 func init() {
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/tokensignin", TokenSignin)
 }
